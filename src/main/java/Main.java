04ff7954e11155499f4302e3a76219cee80b285b
sasa5680 <sasa5680@naver.com>
2020-07-18 17:30:11 +0900

    add prefix folders

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import com.google.protobuf.Any;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.messageClass.*;
 
 public class Main {
 	
 
 		
-	public static void main(String args[]) {
-			
+	public static void main(String args[])  {
+		
+		
 		
 		
 	}
