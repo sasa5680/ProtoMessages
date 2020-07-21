398942aa294684449e164f67426c449db6a4abf4
sasa5680 <sasa5680@naver.com>
2020-07-21 15:42:18 +0900

    sss

--- src/main/java/com/sasa5680/CommonIndex/Network.java
+++ src/main/java/com/sasa5680/CommonIndex/Network.java
@@ -5,5 +5,9 @@ public class Network {
 	public static final int PingPong_Interval = 15;
 	
 	public static final int MAXIMUM_NetworkUnstableCount = 5;
+	
+	public static final int port = 8001;
+	
+	public static final String IP = "127.0.0.1";
 
 }
